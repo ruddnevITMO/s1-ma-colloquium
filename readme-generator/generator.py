63cdda49404b4c16b82c922cbf0aa535994c5b5e
Nikita <37450057+ruddnev@users.noreply.github.com>
2023-01-11 22:09:42 +0300

    updating generator from ruddnevitmo/cardsite

--- readme-generator/generator.py
+++ readme-generator/generator.py
@@ -1,26 +1,64 @@
 from urllib.parse import quote
 import os
+import sys
+import re
 
 if __name__ == '__main__':
-    writeToReadme = True
-    header = "Материалы для подготовки к коллоквиуму по математическому анализу"
-    githubName = "ruddnevITMO"
-    repoName = "s1-ma-colloquium"
 
-    with open("cardsList.txt", encoding="utf-8") as namesFile:
-        cards = ["0"] + [i.rstrip() for i in namesFile]
+    if len(sys.argv) == 1:
+        # НАЧАЛО ЧАСТИ, КОТОРУЮ НУЖНО ИЗМЕНИТЬ
+
+        githubName = "ваш github ник"
+        repoName = "название вашего репозитория"
+        header = "заголовок вашего сайта"
+        fileExtension = ".jpg"
+
+        # КОНЕЦ ЧАСТИ, КОТОРУЮ НУЖНО ИЗМЕНИТЬ
+    elif len(sys.argv) == 5:
+        githubName = sys.argv[1]
+        repoName = sys.argv[2]
+        header = sys.argv[3]
+        fileExtension = sys.argv[4]
+    else:
+        print("Неверное количество аргументов")
+        sys.exit()
+
+    altFileExtension = fileExtension
+    obfuscateContents = True
+    writeToReadme = True
+    printing = False
+
+    cards = ["0"]
+    with open("../cardsList.txt", encoding="utf-8") as namesFile:
+        for line in namesFile:
+            if line.strip() == "":
+                print("Нашел пустую строку в cardsList.txt")
+            else:
+                cards.append(line.rstrip())
+
+    if re.search(r'^[0-9]+\.', cards[1]):
+        if cards[1][0] != "1":
+            print("Неправильный список билетов в cardsList.txt! Уберите нумерацию")
+
+    for i in range(1, len(cards)):
+        if re.search(r'^[0-9]+\.', cards[i]):
+            cards[i] = re.search(r"(?<=\. ).*", cards[i]).group()
 
     if writeToReadme:
         readme = open("../README.md", "w", encoding='utf-8')
+
 
     def printWrite(*args):
         line = ' '.join([str(arg) for arg in args])
         if writeToReadme:
             readme.write(line + '\n')
-        print(line)
+        if printing:
+            print(line)
+
 
     def quoteConvert(text):
         return text.lower().replace(" ", "-").replace(".", "").replace(",", "").replace("(", "").replace(")", "")
+
 
     replacements = [["о", "o"], ["М", "M"], ["е", "e"],
                     ["а", "a"], ["р", "p"], ["Т", "T"],
@@ -31,7 +69,13 @@ if __name__ == '__main__':
     linkTemplate = "https://" + githubName + ".github.io/" + repoName + "/#"
     linkToTop = linkTemplate + quote(quoteConvert(header))
 
-    alts = [int(i[:-4]) for i in os.listdir("../altCards")]
+    alts = []
+
+    altsDir = os.listdir("../altCards")
+    for file in altsDir:
+        if re.search(r"[0-9]+\.(jpg|png|jpeg)", file):
+            alts.append(int(re.match(r"[0-9]+(?=\.(jpg|png|jpeg))", file).group()))
+
     altsLink = "https://raw.githubusercontent.com/" + githubName + "/" + repoName + "/main/altCards/"
 
     # Заголовок
@@ -42,8 +86,10 @@ if __name__ == '__main__':
     for n in range(1, howManyCards + 1):
         name = cards[n]
         original = name
-        for placing in replacements:
-            name = name.replace(placing[0], placing[1]).replace(placing[0].upper(), placing[1].upper())
+
+        if obfuscateContents:
+            for placing in replacements:
+                name = name.replace(placing[0], placing[1]).replace(placing[0].upper(), placing[1].upper())
 
         outputLine = "[" + name + "](" + linkTemplate + quote(quoteConvert(original) + "-" + str(n) + "-наверх") + ")"
         if n != howManyCards:  # на последней главе содержания не нужен перенос строки
@@ -54,8 +100,9 @@ if __name__ == '__main__':
     # Формируем сами билеты
     for n in range(1, howManyCards + 1):
         printWrite("##", cards[n], "[" + str(n) + "] [наверх](" + linkToTop + ")")
-        printWrite("!["+cards[n]+"]("+imageLinkTemplate+str(n)+".jpg)")
+        printWrite("![" + cards[n] + "](" + imageLinkTemplate + str(n) + fileExtension + ")")
         if n in alts:
             printWrite("### Альтернативный билет для ", cards[n], "[" + str(n) + "] [наверх](" + linkToTop + ")")
-            printWrite("!["+cards[n]+"]("+altsLink+str(n)+".png)")
+            printWrite("![" + cards[n] + "](" + altsLink + str(n) + altFileExtension + ")")
         printWrite("---")
+
